39234953181f5286d4f9d71d909e514056f7e8bc
infospot <infospot112@gmail.com>
2025-06-19 09:30:31 +0000

    refactor

--- js/site.js
+++ js/site.js
@@ -1,4 +1,5 @@
 import { recruitmentRoute, careServicesRoute } from "./index.js";
+
 const recruitment = document.getElementById("recruitment");
 const careservices = document.getElementById("care-services");
 const readmore = document.getElementById("read-more");
@@ -6,20 +7,20 @@ const readmore = document.getElementById("read-more");
 recruitment.addEventListener("click",function(event){
 	event.preventDefault();
 
-	document.writeln("<span class='spinner-border text-info'></span> Loading...");
+	document.writeln("Loading... please wait");
 	recruitmentRoute();
 });
 
 careservices.addEventListener("click",function(event){
 	event.preventDefault();
 
-	document.writeln("<span class='spinner-border text-info'></span> Loading...");
+	document.writeln("Loading... please wait");
 	careServicesRoute();
 });
 
 readmore.addEventListener("click",function(event){
 	event.preventDefault();
 
-	document.writeln("<span class='spinner-border text-info'></span> Loading...");
+	document.writeln("Loading... please wait");
 	careServicesRoute();
 });
