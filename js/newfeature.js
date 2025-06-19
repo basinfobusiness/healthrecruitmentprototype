6e9c52d7f0671ab86a9946eaa2cc2333b00d9056
infospot <infospot112@gmail.com>
2025-06-19 15:02:32 +0000

    refactor

--- js/newfeature.js
+++ js/newfeature.js
@@ -1,3 +1,7 @@
+const statusreport = document.getElementById("status-report");
+
+statusreport.innerHTML += `<small class='text-muted'>under maintainance</small>`;
+
 $(window).on('load', function() {
         $('#salutationModal').modal('show');
 });
