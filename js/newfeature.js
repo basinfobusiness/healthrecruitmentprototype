6eafde52db04d8504ad43a6c0e6e4ba44b35740d
infospot <infospot112@gmail.com>
2025-06-19 15:04:28 +0000

    Update newfeature.js

--- js/newfeature.js
+++ js/newfeature.js
@@ -1,6 +1,6 @@
 const statusreport = document.getElementById("status-report");
 
-statusreport.innerHTML += `<small class='text-muted'>under maintainance</small>`;
+statusreport.innerHTML += `<small class='text-muted'> under maintainance</small>`;
 
 $(window).on('load', function() {
         $('#salutationModal').modal('show');
