8495a86d6557b774a363fbe3781a3a9aa80694bc
infospot <infospot112@gmail.com>
2025-06-22 21:13:48 +0000

    refactor

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,13 @@ import { applyNowFunction } from "./index.js";
 
 const currentDate = new Date();
 const serviceslist = document.querySelectorAll(".about-services-list li");
+const currentYear = currentDate.getFullYear();
+const yearLabel = document.querySelector("#displayYear");
+const carouimage = document.querySelectorAll(".main-carousel");
 
 // to get current year
 function getYear() {
-    let currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerText = currentYear;
+    yearLabel.textContent = currentYear;
 }
 
 (()=>{
@@ -17,12 +19,23 @@ function getYear() {
         item.style.opacity = 1;
     }, index * 200); // Adjust the animation speed
 });
-
+    userOs()
+    console.log(navigator.userAgentData);
 })();
 
 function applyNow(){
     document.writeln("Loading... please wait");
     applyNowFunction();
+}
+
+function userOs(){
+     if (navigator.userAgent == "Android") {
+        for (let i = carouimage.length - 1; i >= 0; i--) {
+            carouimage[i].src = "./images/mobile-healthservices.png";
+        }
+    }else{
+        console.log("nothing to show OS: " + navigator.userAgent);
+    }
 }
 
 // setTimeout(() => {
--- js/site.js
+++ js/site.js
@@ -2,6 +2,11 @@ import { recruitmentRoute, careServicesRoute } from "./index.js";
 
 const recruitment = document.getElementById("recruitment");
 const readmore = document.getElementById("read-more");
+const clientCountElement = document.getElementById('happy-clients');
+const memberCountElement = document.getElementById('member-active');
+const reviewsCountElement = document.getElementById('client-reviews');
+const experienceCountElement = document.getElementById('experience');
+const commitment = document.getElementById('our-commitment');
 
 recruitment.addEventListener("click",function(event){
 	event.preventDefault();
@@ -17,3 +22,29 @@ readmore.addEventListener("click",function(event){
 	careServicesRoute();
 });
 
+const clientCount = 1000; // Replace with your actual client count
+
+function animateCount() {
+  let currentCount = 0;
+  const interval = setInterval(() => {
+    currentCount+=200;
+    clientCountElement.textContent = currentCount;
+    memberCountElement.textContent = currentCount;
+    reviewsCountElement.textContent = currentCount;
+    experienceCountElement.textContent = currentCount;
+    
+    if (currentCount === 1000) {
+    	clientCountElement.textContent = "3K+";
+    	memberCountElement.textContent = "1.7K+";
+    	reviewsCountElement.textContent = "4.7K+";
+    	experienceCountElement.textContent = "6+";
+    }
+    if (currentCount >= clientCount) {
+      clearInterval(interval);
+    }
+  }, 30); // Adjust the interval time to control the animation speed
+}
+
+commitment.addEventListener('mouseover', () => {
+    animateCount();
+});
